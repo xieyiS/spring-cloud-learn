43347e85b78adc522e94f31118a79686e4983f91
sunxieyi <540416364@qq.com>
2021-01-15 17:03:15 +0800

    1.gateway动态路由 2.config服务端

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -63,4 +63,9 @@ public class PaymentController {
         }
         return serverPort;
     }
+
+    @GetMapping(value = "/payment/lb")
+    public String getPaymentLB() {
+        return serverPort;
+    }
 }
